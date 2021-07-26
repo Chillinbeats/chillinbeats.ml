c86da79b8f66d7f32ecafba6712d167174b74c41
AvIsBeastMC <admin@playavmcgames.ml>
2021-07-26 12:46:06 +0530

    Hey

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
+
 async function requestAuthorization() {
-  $.getJSON("/json/auth.json", function(json) {
-    console.log(json); // this will show the info i
+  $.getJSON("https://quicktimes.ml/json/auth.json", function(json) {
+    console.log(json);
+    localStorage.setItem("api_token", json.api_token)
+    localStorage.setItem("client_id", json.client_id)
+    localStorage.setItem("client_secret", json.client_secret)
+
 });
 }
+const delay = ms => new Promise(res => setTimeout(res, ms));
+    const checkItems = async () => {
+        await delay(3350);
+        requestAuthorization()
+  }
